0315e63fc0ee65cc279e30588a55ae41096182a3
amatzar0 <118388154+amatzar0@users.noreply.github.com>
2022-11-30 12:56:02 -0600

    changes to controller and view
    changed a couple read only from True to False.
    Updated functions submit, clear, and calculate for logic of split bill and custom tip.

--- controller.py
+++ controller.py
@@ -4,14 +4,13 @@ from view import *
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-
 def calculate(food, drink, dessert, tip):
     tax = 0.1
     total_tax = (food + drink + dessert) * tax
-    if tip[0] == 'percent':
+    if 'percent' in tip[0]:
         total_tip = (food + drink + dessert + total_tax) * tip[1]
     else:
-        total_tip = food + drink + dessert + total_tax + tip[1]
+        total_tip = tip[1]
     grand_total = food + drink + dessert + total_tax + total_tip
     return total_tax, total_tip, grand_total
 
@@ -42,23 +41,36 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.Summary_Label.setText('')
         self.Custom_Entry.setText('')
         self.Custom_Entry.setReadOnly(True)
-
+        self.Split_Checkbox.setChecked(False)
+        self.Split_Entry.setValue(1)
+    
     def submit(self):
-        try:
-            food = float(self.Food_Entry.text())
-            drink = float(self.Drink_Entry.text())
-            dessert = float(self.Dessert_Entry.text())
+        if (self.Radio_10.isChecked()) or (self.Radio_15.isChecked()) or (self.Radio_20.isChecked()):
             if self.Radio_10.isChecked():
                 tip = ['percent', .1]
             elif self.Radio_15.isChecked():
                 tip = ['percent', .15]
-            elif self.Radio_20.isChecked():
-                tip = ['percent', .2]
-            elif self.Radio_Custom.isChecked():
-                tip = ['amount', float(self.Custom_Entry.text())]
             else:
-                tip = 0
+                tip = ['percent', .2]
+        else:
+            tip = ['amount' ,float(self.Custom_Entry.text())]
+
+        try:
+            food = float(self.Food_Entry.text())
+            drink = float(self.Drink_Entry.text())
+            dessert = float(self.Dessert_Entry.text())
             total_tax, total_tip, grand_total = calculate(food, drink, dessert, tip)
-            self.Summary_Label.setText(f'{" " * 25}SUMMARY\n\nFood:\t\t${food:.2f}\nDrink:\t\t${drink:.2f}\nDessert:\t\t${dessert:.2f}\nTax:\t\t${total_tax:.2f}\nTip:\t\t${total_tip:.2f}\n\nTOTAL:\t\t${grand_total:.2f}')
         except ValueError:
             self.Summary_Label.setText(f'\n\n\n{" " * 15}Food drink, and dessert\n{" " * 15}need to be numeric\n{" " * 15}e.g. 10.25 not $10.25')
+        
+        if self.Split_Checkbox.isChecked():
+            num_people = self.Split_Entry.value()
+            try:    
+                split_check = grand_total / num_people
+                self.Summary_Label.setText(f'{" " * 25}SUMMARY\n\nFood:\t\t${food:.2f}\nDrink:\t\t${drink:.2f}\nDessert:\t\t${dessert:.2f}\nTax:\t\t${total_tax:.2f}\nTip:\t\t${total_tip:.2f}\n\nTOTAL:\t\t${grand_total:.2f}\nEach Pay:\t${split_check:.2f}')
+            except:
+                self.Summary_Label.setText(f'\n\n\n{" " * 15}Food drink, and dessert\n{" " * 15}need to be numeric\n{" " * 15}e.g. 10.25 not $10.25')
+        else:
+            total_tax, total_tip, grand_total = calculate(food, drink, dessert, tip)
+            self.Summary_Label.setText(f'{" " * 25}SUMMARY\n\nFood:\t\t${food:.2f}\nDrink:\t\t${drink:.2f}\nDessert:\t\t${dessert:.2f}\nTax:\t\t${total_tax:.2f}\nTip:\t\t${total_tip:.2f}\n\nTOTAL:\t\t${grand_total:.2f}')
+        
